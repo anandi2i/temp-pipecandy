e07e0287274c1a60c1fdbcec98dbfe2b4b052cf3
dineshi2it <dinesh.r@ideas2it.com>
2016-03-30 15:39:53 +0530

    Feature: Listing people with related fields for given lists id

--- common/models/list.js
+++ common/models/list.js
@@ -1,3 +1,42 @@
+import logger from "../../server/log";
+
 module.exports = function(List) {
-//method
+  /**
+   * Get all People and its related field data for the given lists id
+   * @param ctx
+   * @param options
+   * @param cb
+   */
+  List.listPeopleField = function(ctx, options, cb) {
+    List.find({
+      where: {id: {inq: options.list}, createdBy: ctx.req.accessToken.userId},
+      include: {"people" : ["fields"]}
+    }, (err, people) => {
+      if(err) {
+        logger.error("Error in getting people data for lists", options.list);
+        return cb(err);
+      }
+      cb(null, people);
+    });
+  };
+
+  List.remoteMethod(
+    "listPeopleField",
+    {
+      description: "List all the people and its field data for given list id's",
+      accepts: [
+        {
+          arg: "ctx", type: "object",
+          http: {source: "context"}
+        },
+        {
+          arg: "options", type: "object", required: true,
+          http: {source: "body"}
+        },
+      ],
+      returns: {arg: "peopleData", type: "object", root: true},
+      http: {verb: "post"}
+    }
+  );
+
 };
