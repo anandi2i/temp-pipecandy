7b39fc800765f119eb74921592c0bd5df1ea4784
SyedSulaimanM <syedsulaiman.m@ideas2it.com>
2016-06-22 17:04:31 +0530

    Feature: Added configuration file for queue properties

--- server/utils/queue-config.js
+++ server/utils/queue-config.js
@@ -1,14 +1,17 @@
+var config = require("../../server/config.json");
+
 /**
- * List of public email providers
+ * Queue Configuration File
+ * @author Syed Sulaiman M
  */
-const accessKeyId = "AKIAJ53GEQAAN6LWLTUQ";
-const secretAccessKey = "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";
-const region = "ap-southeast-1";
+const accessKeyId = config.queue.config.accessKeyId;
+const secretAccessKey = config.queue.config.secretAccessKey;
+const region = config.queue.config.region;
 
 const queueUrl = {
-  mailNLPIdentification: "https://sqs.ap-southeast-1.amazonaws.com/736447789897/pc-dev-intelligence-in",
-  mailNLPDetermined: "https://sqs.ap-southeast-1.amazonaws.com/736447789897/pc-dev-intelligence-out",
-  mailAssemblerQueue: "https://sqs.ap-southeast-1.amazonaws.com/736447789897/pc-dev-email-assembler"
+  mailNLPIdentification: config.queue.url.intelligenceIn,
+  mailNLPDetermined: config.queue.url.intelligenceOut,
+  mailAssemblerQueue: config.queue.url.emailAssembler
 };
 
 module.exports = {
