24b402c23b6180a173e7973daebae06cf3853d2c
dineshi2it <dinesh.r@ideas2it.com>
2016-07-27 10:59:31 +0530

    Fix: Pagination doesn't work after 50 lists Bug url: 	- https://pipecandy.atlassian.net/browse/AUT-107 	- https://pipecandy.atlassian.net/browse/AUT-109

--- client/components/grid/CustomGridPagination.react.js
+++ client/components/grid/CustomGridPagination.react.js
@@ -1,3 +1,7 @@
+/**
+ * @see: http://griddlegriddle.github.io/Griddle/customization.html
+ */
+
 import React from "react";
 
 class CustomGridPagination extends React.Component {
@@ -7,70 +11,45 @@ class CustomGridPagination extends React.Component {
   }
 
   pageChange = (e) => {
-    // value starts from 0... maxPage-1
-    this.props.setPage(parseInt(e.target.getAttribute("data-value"), 5));
+    this.props.setPage(parseInt(e.target.getAttribute("data-value")));
   }
 
   render() {
+    const {currentPage, maxPage} = this.props;
     let lists = [];
-    let defaultStartIndex = 0;
-    // update start index at after 4th button click but it's value is 3.
-    // Pagination Value starts from 0...maxpage-1
-    let startIndexPoint = 3;
-    let endIndexPoint = 5;
-    // Max button to be showed is 6
-    let noOfButtonShow = 6;
-    let startIndex = Math.max(
-      this.props.currentPage - startIndexPoint, defaultStartIndex
-    );
-    let endIndex = Math.min(startIndex + noOfButtonShow, this.props.maxPage);
-    if (this.props.maxPage >= noOfButtonShow &&
-        (endIndex - startIndex) <= endIndexPoint) {
-      startIndex = endIndex - startIndexPoint;
+    const defaultStartIndex = 0;
+    const startIndexPoint = 3;
+    const endIndexPoint = 6;
+    const noOfButtonShow = 7;
+    let startIndex = Math.max(currentPage - startIndexPoint, defaultStartIndex);
+    let endIndex = Math.min(startIndex + noOfButtonShow, maxPage);
+    if(maxPage >= noOfButtonShow && (endIndex - startIndex) <= endIndexPoint) {
+      startIndex = endIndex - noOfButtonShow;
     }
-
-    let checkIfOnePage = 1;
+    const onePage = 1;
     let increment = 1;
-    if (this.props.maxPage !== checkIfOnePage) {
-      for (let i = startIndex; i < endIndex; i++) {
+    if (maxPage !== onePage) {
+      for(let i = startIndex; i < endIndex; i++) {
+        const selected = currentPage === i ? "pager-active" : "pager-btn";
         lists.push(
-        <li 
-          key={`page${i}`} 
-          className={
-            this.props.currentPage === i 
-            ? 
-              "pager-active" 
-            : 
-              "pager-btn"
-          }
-          data-value={i}
-          onClick={this.pageChange}>
-          {i + increment}
-        </li>
+          <li key={`page${i}`} className={selected} data-value={i}
+            onClick={this.pageChange}>
+            {i + increment}
+          </li>
         );
       }
     }
-
     return (
-      <div>
-        {
-          lists.length
-          ?
-            <div className="row">
-              <div className="col s12">
-                <ul className="pagination griddle-pager">
-                  <span>Page&nbsp;</span>
-                  {lists}
-                </ul>
-              </div>
-            </div>
-          :
-            ""
-        }
+      <div style={{display: lists.length ? "block" : "none"}} className="row">
+        <div className="col s12">
+          <ul className="pagination griddle-pager">
+            <span>Page&nbsp;</span>
+            {lists}
+          </ul>
+        </div>
       </div>
     );
   }
-
 }
 
 CustomGridPagination.defaultProps = {
