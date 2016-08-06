648d9964e45c96f41e39d7eaab4b78aeba2a3383
dineshi2it <dinesh.r@ideas2it.com>
2016-08-06 20:11:57 +0530

    AUT-242 - feature: environment specific config for uat

--- build/dev.sh
+++ build/dev.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-#build.sh
+#dev.sh
 #author: dinesh.r@ideas2it.com
 
+export NODE_ENV="dev"
+
 #Get the branch name from user
-read -p "Which branch to pull (dev/master)? " branch_name
+read -p "Which branch to pull (dev/master/demo)? " branch_name
 
 #Ask if automigrate is required
 read -p "Do you need to run automigrate (y/n)? " automigrate
 
 echo "-------------------------------------------------------------------------"
 #Pull dev code
-if [ "master" = "$branch_name" ] || [ "dev" = "$branch_name" ]; then
+if [ "master" = "$branch_name" ] || [ "dev" = "$branch_name" ] || [ "demo" = "$branch_name" ]; then
   echo "Pulling the branch $branch_name from pipecandy repo"
   git pull origin $branch_name
   echo "Successfully pulled out the code"
@@ -24,23 +26,6 @@ echo "-------------------------------------------------------------------------"
 pm2 stop all
 echo "Application has been stopped"
 echo "-------------------------------------------------------------------------"
-
-: '
-#Find and kill the running process
-#http://unix.stackexchange.com/questions/37313/how-do-i-grep-for-multiple-patterns
-SERVICES="campaignConsumer\|followUpSender\|emailQueuePoller\|campaignMailSender\|mailDequeue\|initEmailReader"
-#http://stackoverflow.com/questions/13910087/shell-script-to-capture-process-id-and-kill-it-if-exist
-PID=`ps -eaf | grep $SERVICES | grep -v grep | awk "{print $2}"`
-if [ "" !=  "$PID" ];
-then
-  echo "killing the process like campaignConsumer, followUpSender, emailQueuePoller"
-  kill -9 $PID
-  echo "Killed the running process sucessfully"
-else
-  echo "No process found to kill"
-fi
-echo "-------------------------------------------------------------------------"
-'
 
 #Install the node modules
 echo "Installing node modules"
@@ -72,23 +57,6 @@ echo "Bundling the front end code"
 npm run build
 echo "Successfully bundled the front end code"
 echo "-------------------------------------------------------------------------"
-
-export NODE_ENV="dev"
-
-: '
-Start the services/process
-http://stackoverflow.com/questions/23024850/nohup-as-background-task-does-not-return-prompt
-echo "Starting the campaign sending services campaignConsumer, followUpSender, emailQueuePoller"
-nohup npm run campaignConsumer > log/campaignConsumer.out 2>&1 &
-nohup npm run campaignMailSender > log/campaignMailSender.out 2>&1 &
-nohup npm run mailDequeue > log/mailDequeue.out 2>&1 &
-nohup npm run initEmailReader > log/initEmailReader.out 2>&1 &
-nohup npm run followUpSender > log/followUpSender.out 2>&1 &
-nohup npm run emailQueuePoller > log/emailQueuePoller.out 2>&1 &
-
-echo "Successfully started the process"
-echo "-------------------------------------------------------------------------"
-'
 
 #Start pm2
 echo "Starting the application"
