ae1422d7cf7c63d2bf98b85d02d449265e81da25
aswin-ideas2it <aswin@ideas2it.com>
2016-09-26 12:51:34 +0530

    AUT-416 Save template with followups  - Modified the code in DefaultTemplate.js to save followups  - Modifed the code to get all the default templates with followups

--- common/models/default-template.js
+++ common/models/default-template.js
@@ -2,6 +2,8 @@
 
 import {errorMessage as errorMessages} from "../../server/utils/error-messages";
 import logger from "../../server/log";
+import lodash from "lodash";
+import async from "async";
 
 module.exports = function(DefaultTemplate) {
 
@@ -21,11 +23,29 @@ module.exports = function(DefaultTemplate) {
 
   /**
    * Method to save Campaign Template
+   * sampleObjInput: {
+     name: "Save template",
+     content: "<div>Test save template</div>",
+     followUps: [
+       {
+         stepNo: 1,
+         content: "<div>Test save followUp 1 template</div>"
+       },
+       {
+         stepNo: 2,
+         content: "<div>Test save followUp 2 template</div>"
+       },
+       {
+         stepNo: 3,
+         content: "<div>Test save followUp 3 template</div>"
+       }
+     ]
+   }
    * @param  {Context}   ctx
    * @param  {DefaultTemplate}   template
    * @param  {Function} callback
    * @return {DefaultTemplate}
-   * @author Syed Sulaiman M
+   * @author Syed Sulaiman M, Aswin Raj(Modified)
    */
   DefaultTemplate.saveTemplate = (ctx, template, callback) => {
     let userId = ctx.req.accessToken.userId;
@@ -36,14 +56,37 @@ module.exports = function(DefaultTemplate) {
     if(!templateJSON.content) {
       return callback(errorMessages.BLANK_TEMPLATE_CONTENT);
     }
-    templateJSON.createdBy = userId;
-    delete templateJSON.id;
-    DefaultTemplate.create(templateJSON, (error, defaultTemplate) => {
+    const myTemplate = {
+      name: templateJSON.name,
+      content: templateJSON.content,
+      createdBy: userId
+    };
+    let followUps = templateJSON.followUps || [];
+    DefaultTemplate.create(myTemplate, (error, defaultTemplate) => {
       if(error) {
         logger.error({error: error, stack: error.stack});
         return callback(errorMessages.SERVER_ERROR);
       }
-      return callback(null, defaultTemplate);
+      if(lodash.isEmpty(followUps)) return callback(null, defaultTemplate);
+      const followUpObj = followUps.map((followUp) => {
+        followUp.name = templateJSON.name;
+        followUp.createdBy = userId;
+        followUp.parentId = defaultTemplate.id;
+        return followUp;
+      });
+      DefaultTemplate.create(followUpObj, (createErr, followUps) => {
+        if(createErr) {
+          logger.error("Error while creating my followup templates",
+          {error: createErr, stack: createErr.stack});
+          return callback(errorMessages.SERVER_ERROR);
+        }
+        const savedTemplate = {
+          name: defaultTemplate.name,
+          content: defaultTemplate.content,
+          followUps: followUps
+        };
+        return callback(null, savedTemplate);
+      });
     });
   };
 
@@ -101,7 +144,7 @@ module.exports = function(DefaultTemplate) {
   DefaultTemplate.remoteMethod(
     "myTemplates",
     {
-      description: "Get Campaign Templates",
+      description: "Get all user's saved Templates",
       accepts: [
         {arg: "ctx", type: "object", http: {source: "context"}}
       ],
@@ -115,24 +158,68 @@ module.exports = function(DefaultTemplate) {
    * @param  {Context}   ctx
    * @param  {Function} callback
    * @return {DefaultTemplates}
-   * @author Syed Sulaiman M
+   * @author Syed Sulaiman M, Aswin Raj A(Modified)
    */
   DefaultTemplate.myTemplates = (ctx, callback) => {
     let userId = ctx.req.accessToken.userId;
+    async.waterfall([
+      async.apply(getMyMaintemplates, userId),
+      getFollowUpsforMainTemplates,
+    ], (asyncErr, myDefaultTemplates) => {
+      return callback(null, myDefaultTemplates);
+    });
+  };
+
+  /**
+   * To get all the main defaultTemplates for the current user
+   * @param  {[number]} userId
+   * @param  {[function]} getTemplateCB
+   * @return {[array]} defaultTemplates
+   * @author Aswin Raj A
+   */
+  const getMyMaintemplates = (userId, getTemplateCB) => {
     DefaultTemplate.find({
-      where : {
-        createdBy : userId,
-      },
+      where : {and : [{createdBy : userId}, {parentId: null}]},
       order: "createdAt DESC"
     }, (error, defaultTemplates) => {
       if(error) {
         logger.error({error: error, stack: error.stack,
             input: {userId: userId}});
-        return callback(errorMessages.SERVER_ERROR);
+        return getTemplateCB(errorMessages.SERVER_ERROR);
       }
-      return callback(null, defaultTemplates);
+      return getTemplateCB(null, defaultTemplates);
     });
   };
+
+  /**
+   * To get the followUpTemplates for each defatult template and generate
+   * default Templates with followUps
+   * @param  {[type]} defaultTemplates [description]
+   * @param  {[type]} getFollowUpsCB   [description]
+   * @return {[type]}                  [description]
+   * @author Aswin Raj A
+   */
+  const getFollowUpsforMainTemplates = (defaultTemplates, getFollowUpsCB) => {
+    const templatesWithFollowUp = [];
+    async.eachSeries(defaultTemplates, (template, templateCB) => {
+      DefaultTemplate.find({
+        where : {parentId: template.id},
+        order: "stepNo ASC"
+      }, (error, followUpTemplates) => {
+        if(error) {
+          logger.error({error: error, stack: error.stack,
+              input: {userId: userId}});
+          return callback(errorMessages.SERVER_ERROR);
+        }
+        template.followUps = followUpTemplates;
+        templatesWithFollowUp.push(template);
+        return templateCB(null);
+      });
+    }, (asyncErr) => {
+      return getFollowUpsCB(asyncErr, templatesWithFollowUp);
+    });
+  };
+
 
   DefaultTemplate.remoteMethod(
     "templates",
